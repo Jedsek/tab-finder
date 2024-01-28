ffdf094dcd1de4f082d103862a04a74cdac084eb
jedsek <jedsek@qq.com>
2024-01-28 15:09:04 +0800

    update

---
+++ README.md
@@ -0,0 +1,42 @@
+# tab-finder
+
+This is a [zellij](zellij.dev) plugin for quickly switching to specific tab by inputing its name, with beautiful UI.
+
+# Show
+
+![showcase-1](showcase/1.png)  
+![showcase-2](showcase/2.png)  
+
+# Config
+
+```kdl
+// ~/.config/zellij/config.kdl
+keybinds {
+    // ......
+    // ......
+    shared_except "locked" {
+        bind "Alt t" {
+            LaunchOrFocusPlugin "file:~/.config/zellij/plugins/tab-finder.wasm" {
+                floating true
+                ignore_case true
+            }
+        }
+    }
+}
+```
+
+# How to get it?
+
+## Release
+
+The precompiled `.wasm` file could be found in [release](https://github.com/Jedsek/tab-finder/releases/)
+
+## Manual
+
+1. `rustup target add wasm32-wasi`
+2. `cargo build --release`
+3. `cp target/wasm32-wasi/release/tab-finder.wasm ~/.config/zellij/plugins/`
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,116 @@
+// #![allow(unused)]
+
+use colored::{ColoredString, Colorize};
+use std::collections::BTreeMap;
+use zellij_tile::prelude::*;
+
+#[derive(Default)]
+struct State {
+    tabs: Vec<TabInfo>,
+    filter: String,
+    selected: usize,
+    ignore_case: bool,
+}
+
+register_plugin!(State);
+
+impl ZellijPlugin for State {
+    fn load(&mut self, configuration: BTreeMap<String, String>) {
+        // ReadApplicationState permission to receive the ModeUpdate and TabUpdate events
+        // ChangeApplicationState permission to Change Zellij state (Panes, Tabs and UI)
+        request_permission(&[PermissionType::ReadApplicationState, PermissionType::ChangeApplicationState]);
+
+        self.ignore_case = match configuration.get("ignore_case") {
+            Some(value) => value.trim().parse().unwrap(),
+            None => true,
+        };
+
+        subscribe(&[EventType::TabUpdate, EventType::Key]);
+    }
+
+    fn update(&mut self, event: Event) -> bool {
+        let mut should_render = false;
+        let selected = self.selected;
+
+        match event {
+            Event::TabUpdate(tab_info) => {
+                self.tabs = tab_info;
+                should_render = true;
+            }
+            Event::Key(Key::Esc | Key::Ctrl('c')) => close_focus(),
+            Event::Key(Key::Up) => {
+                self.selected = if selected == 0 { self.tabs.len() - 1 } else { selected - 1 };
+                should_render = true;
+            }
+            Event::Key(Key::Down) => {
+                self.selected = if selected == self.tabs.len() - 1 { 0 } else { selected + 1 };
+                should_render = true;
+            }
+            Event::Key(Key::Char('\n')) => {
+                close_focus();
+                switch_tab_to(self.selected as u32 + 1);
+            }
+            Event::Key(Key::Backspace) => {
+                self.filter.pop();
+                if self.filter.is_empty() {
+                    self.selected = 0;
+                }
+                should_render = true;
+            }
+            Event::Key(Key::Char(c)) => {
+                self.filter.push(c);
+                should_render = true;
+            }
+            _ => (),
+        };
+
+        should_render
+    }
+
+    fn render(&mut self, _rows: usize, _cols: usize) {
+        let filter = self.filter.as_str();
+
+        let condition = |tab: &&TabInfo| {
+            if self.ignore_case {
+                tab.name.clone().to_lowercase().contains(&filter.to_lowercase())
+            } else {
+                tab.name.contains(filter)
+            }
+        };
+
+        let prompt = if filter.is_empty() {
+            String::new()
+        } else {
+            self.selected = self.tabs.iter().find(condition).map(|tab| tab.position).unwrap_or(0);
+            format!("{} {}\n", ">".magenta(), filter.bright_green().italic())
+        };
+
+        let to_line = |tab: &TabInfo| {
+            let line_text = format!(
+                "{}, {} {}",
+                tab.position + 1,
+                &(tab.name),
+                if tab.active { "(*)" } else { "" },
+            )
+            .cyan();
+
+            if tab.position == self.selected {
+                line_text.on_bright_magenta().black()
+            } else {
+                line_text
+            }
+        };
+
+        let tabs = self
+            .tabs
+            .iter()
+            .filter(condition)
+            .map(to_line)
+            .collect::<Vec<ColoredString>>();
+
+        print!("{}", prompt);
+        for tab in tabs {
+            println!("{}", tab)
+        }
+    }
+}
